8fe963223b272badd3c638adb873c3f1531efcd4
Rahul Rathod <65287267+rahulmns@users.noreply.github.com>
2022-07-21 17:44:32 +0100

    First Commit

---
+++ Morning/Shared/MorningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorningApp.swift
+//  Shared
+//
+//  Created by 1390411 on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MorningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
